a9f308ce2292ccc3a9d732d67d774da566cb2790
Lephilosophe237 <jngan077@uottawa.ca>
2023-07-02 12:50:38 -0400

    decond commit

---
+++ build/precache-manifest.8daf2b63f44a65387aad710c0997761b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39216fef0364875c368d21addf497eaa",
+    "url": "/reactAppGokart/index.html"
+  },
+  {
+    "revision": "f356e46711cefaad617d",
+    "url": "/reactAppGokart/static/css/2.6ea853fb.chunk.css"
+  },
+  {
+    "revision": "9931fe875be1c7b16ff5",
+    "url": "/reactAppGokart/static/css/main.a973efef.chunk.css"
+  },
+  {
+    "revision": "f356e46711cefaad617d",
+    "url": "/reactAppGokart/static/js/2.6691ef15.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/reactAppGokart/static/js/2.6691ef15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9931fe875be1c7b16ff5",
+    "url": "/reactAppGokart/static/js/main.5c1586ab.chunk.js"
+  },
+  {
+    "revision": "d20e77fd111b7c1dc7af",
+    "url": "/reactAppGokart/static/js/runtime-main.98ce1d1b.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/reactAppGokart/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/reactAppGokart/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/reactAppGokart/static/media/revicons.2feb69cc.eot"
+  }
+]);
